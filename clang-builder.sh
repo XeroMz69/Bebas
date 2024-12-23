159e33ced310d1984091413f97e84dfb7a86477d
Adam YD <adamyd18@gmail.com>
2024-12-23 22:18:15 +0700

    clang: fixup!

--- clang-builder.sh
+++ clang-builder.sh
@@ -186,7 +186,7 @@ if [[ "$fail" == "n" ]];then
     cp -f $WORKDIR/out/README.md $WORKDIR/.
     cd $WORKDIR/Clang
     git add .
-    git commit -sSm "$(date +"%Y%m%d")" -m "$(cat $WORKDIR/README.md)"
+    git commit -sm "$(date +"%Y%m%d")" -m "$(cat $WORKDIR/README.md)"
     git show
     git push --all origin -f
     popd || exit
